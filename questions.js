9a3626dc07afa59e0c0196a0fb832a270d9e0098
br8832 <bryan2098@hotmail.com>
2021-05-13 18:51:13 -0700

    added questions.js

---
+++ questions.js
@@ -0,0 +1,55 @@
+//using node.js's fetch isn't working, so it's something to fix later
+//import fetch from 'node-fetch';
+
+//base link for requsts to the api
+const opentdb = 'https://opentdb.com/api.php?';
+const jeopardy = 'https://jeopardy-api.bentleyherron.dev/api/';
+
+//these are the modifiers for opentbd
+let opentdb_options = {
+    amount: "",//max of 50
+    category: "",//9-32 maybe
+    type:"", //can be multiple or boolean
+}
+let jeopardy_options = {
+    shows: "",//0-3639
+    categories: ""//retrieve all categories (too many to list)
+}
+// in the call, specify the opentbd parameters
+// you always need to specify amount
+async function opentdbQuestions(amount, category, type)
+{
+    //append the api call
+    var append = "";
+    if(amount)
+        append += `amount=${amount}`;
+    if(category)
+        append += `&category=${category}`;
+    if(type)
+        append += `type=${type}`;
+    console.log(append);
+    const response = await fetch(`https://opentdb.com/api.php?${append}`);
+    const data = await response.json();
+    return data;
+}
+// in the call, specify jeopardy paramaters
+// realisticall for jeopardy, we'll do random numbers
+async function jeopardyQuestionsbyShow (showNumber)
+{
+    const response = await fetch(`https://jeopardy-api.bentleyherron.dev/api/shows/${showNumber}`);
+    const data = await response.json();
+    return data;
+}
+async function jeopardyQuestionsbyCategories (category)
+{
+    const response = await fetch(`https://jeopardy-api.bentleyherron.dev/api/categories/${category}`);
+    const data = await response.json();
+    return data;
+}
+
+//this holds all the quesions globally
+//although to work with it requires funky buisiness
+//I'll see if there is an easier way later
+var opentbdTest = opentdbQuestions(30, "multiple");
+var jeopardyByCategory = jeopardyQuestionsbyCategories(1);
+var jeopardyByShow = jeopardyQuestionsbyShow(4000);
--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-var result;
-fetch('https://opentdb.com/api.php?amount=10&category=24')
-.then(res => {result = res.json()})
-.then(json => console.log(json));
-console.log(result)
+let Step1 = fetch('https://opentdb.com/api.php?amount=10&category=24')
+let Step2 = Step1.then(res => res.json())
+let Step3 = Step2.then(json => {
+    let q = json.results;
+    let body = document.querySelector('body');
+    q.forEach(element => {
+        let p = document.createElement('p');
+        p.innerHTML = element.question;
+        body.appendChild(p);
+    });
+})
+let jeopardy1 = fetch('https://jeopardy-api.bentleyherron.dev/api/shows');
+let jeopardy2 = jeopardy1.then(res => res.json())
+let jeopardy3 = jeopardy2.then(json => {
+    // random category
+    console.log(json.show_numbers.length);
+    let show = json.show_numbers[Math.floor(Math.random() * json.show_numbers.length)]
+    fetch('https://jeopardy-api.bentleyherron.dev/api/shows/'  + show).then(
+        res => res.json()).then(result => console.log(result))
+})
